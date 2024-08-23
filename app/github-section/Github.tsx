11a50e554adfc391beba256bede6fe1c19981ff1
hvtienprotv84 <dmx031117@gmail.com>
2024-08-24 05:52:55 +0700

    push fix dark mode github section

--- app/github-section/Github.tsx
+++ app/github-section/Github.tsx
@@ -2,22 +2,17 @@ import "../animations/animate.css";
 import { motion } from "framer-motion";
 import { imageAnimation } from "../animations/animations";
 import GitHubCalendar from 'react-github-calendar';
-import clsx from 'clsx';
-import { useTheme } from 'next-themes';
+// import clsx from 'clsx';
+// import { useTheme } from 'next-themes';
 
 export const Github = () => {
-  const { theme } = useTheme();
+  // const { theme } = useTheme();
 
   return (
-    <motion.section
-      className={clsx(
-        'mt-[50px] lg:mt-[0px] lg:pb-[100px]',
-        theme === 'dark' ? 'bg-white' : 'bg-black'
-      )}
-    >
-      <p className={clsx('w-full max-w-[350px] lg:w-full lg:max-w-full', theme === 'dark' ? 'bg-white' : 'bg-black' )}>
-        <GitHubCalendar username="hvtienprotv84" />  
-      </p>
+    <motion.section className='mt-[50px] lg:mt-[0px] lg:pb-[150px] lg:h-fit lg:w-fit max-h-[200px] lg:p-[20px]'>
+        <p className='w-full max-w-[350px] lg:w-full lg:max-w-full'>
+             <GitHubCalendar username="hvtienprotv84" />  
+        </p>
     </motion.section>
   );
 }
