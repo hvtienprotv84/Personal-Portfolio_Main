accb4123d8aed84fd3f7f70402223c2e637014cb
hvtienprotv84 <dmx031117@gmail.com>
2024-08-24 06:31:56 +0700

    push dark mode certification

--- app/blog-section2/BlogGrid.tsx
+++ app/blog-section2/BlogGrid.tsx
@@ -1,12 +1,28 @@
+import React, { useEffect, useState } from 'react';
 import BlogCard from "./BlogCard";
 import { blogDetails } from "./blogDetails";
 import AnimatedWords2 from "../animations/AnimatedWords2";
 import { monaSans } from "../fonts/monaSans";
 import AnimatedBody from "../animations/AnimatedBody";
+import clsx from 'clsx';
+import { useTheme } from 'next-themes';
 
 const Blog = () => {
+  const { theme } = useTheme(); 
+  const [textColor, setTextColor] = useState('');
+  const [bgColor, setBackgroundColor] = useState('');
+
+  useEffect(() => {
+    // Cập nhật màu sắc dựa trên theme
+    setTextColor(theme === 'dark' ? 'text-[#e4ded7]' : 'text-[#ffb800]')
+    setBackgroundColor(theme === 'dark' ? 'bg-[#0E1016]' : 'bg-[#e4ded7]');
+  }, [theme]);  
+
   return (
-    <section className="z-10 flex w-full flex-col items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pt-20 md:pb-16 lg:pb-32">
+    <section className={clsx("z-10 flex w-full flex-col items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pt-20 md:pb-16 lg:pb-32",
+      bgColor  
+      )}
+      >
       <div id="certificate" className="mb-[60px]"></div>
       <div
         className={`relative mb-16 flex w-full flex-col items-center justify-center gap-10  text-[#e4ded7] sm:items-center lg:max-w-[1440px]`}
@@ -14,7 +30,9 @@ const Blog = () => {
         {" "}
         <AnimatedWords2
           title={"certificate"}
-          style={`flex max-w-[500px] pr-5 flex-col items-start text-left ${monaSans.className} font-extrabold uppercase leading-[0.9em] text-[#e4ded7] sm:max-w-full sm:flex-row sm:items-center sm:justify-center sm:text-center lg:text-center text-[clamp(70px,10vw,155.04px)]`}
+          style={clsx(`flex max-w-[500px] pr-5 flex-col items-start text-left ${monaSans.className} font-extrabold uppercase leading-[0.9em] text-[#e4ded7] sm:max-w-full sm:flex-row sm:items-center sm:justify-center sm:text-center lg:text-center text-[clamp(70px,10vw,155.04px)]`,
+            textColor  
+          )}
         />
         {/* <AnimatedBody
           text="I write articles to reinforce my knowledge and help out others who might be building something similar."
